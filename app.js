f7ce67dc771a831b4acc90da55996fc812c36e32
iabdulhakim <abdulhakimisroil@gmail.com>
2023-12-19 09:46:57 +0500

    fixed some bugs

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-const h2El = document.querySelector(".product__count");
-let count = 1;
+const plusBtns = document.querySelectorAll(".product-plus");
+const minusBtns = document.querySelectorAll(".product-minus");
+const totalSpentEl = document.querySelector(".remain-money-number");
 
-btnEl.addEventListener("click", function () {
-    count++;
-    h2El.textContent = count;
+const totalSumma = 200000;
+let totalSpent = 0;
+
+plusBtns.forEach(plusBtn => {
+    plusBtn.addEventListener("click", () => {
+        const price = +plusBtn.parentElement.previousElementSibling.textContent;
+        const divEl = plusBtn.nextElementSibling;
+        if (totalSumma >= totalSpent + price) {
+            totalSpent += price * 1;
+            divEl.textContent++;
+            totalSpentEl.textContent = totalSpent;
+        } else {
+            alert("Pulingiz yetmaydi!");
+        }
+    });
 });
 
-
-
+minusBtns.forEach(minusBtn => {
+    minusBtn.addEventListener("click", () => {
+        const price = minusBtn.parentElement.previousElementSibling.textContent;
+        const divEl = minusBtn.previousElementSibling;
+        if (divEl.textContent > 0) {
+            totalSpent -= price * 1;
+            divEl.textContent--;
+            totalSpentEl.textContent = totalSpent;
+        } else {
+            alert("Pulingiz yetmaydi!");
+        }
+    });
+});
